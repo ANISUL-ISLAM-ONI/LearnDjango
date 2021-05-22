988c118a128996a55d81bd0dd0a13c5867fe92de
ANISUL-ISLAM-ONI <aminjoy82@gmail.com>
2021-05-22 09:22:18 +0600

    Add share, comment and tag sytems in a post

--- LearnDjango/settings.py
+++ LearnDjango/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Dhaka'
 
 USE_I18N = True
 
@@ -124,3 +125,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email SMTP Server Connection Setup
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'everpythonsad@gmail.com'
+EMAIL_HOST_PASSWORD = '@0Niislam400'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
